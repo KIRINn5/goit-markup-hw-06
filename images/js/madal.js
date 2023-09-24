ce7d7289e336ef9e75c68dad64bf9df9a62f2bb1
KIRINn5 <141363578+KIRINn5@users.noreply.github.com>
2023-09-24 19:49:34 +0300

    m

--- images/js/madal.js
+++ images/js/madal.js
@@ -1,14 +1,33 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    // Modal order form
+    openModalBtn: document.querySelector('[open-order-modal-form]'),
+    closeModalBtn: document.querySelector('[close-order-modal-form]'),
+    modalDiv: document.querySelector('[order-modal-form]'),
+
+    // Modal mobile menu
+    openMenuBtn: document.querySelector('[open-mobil-menu]'),
+    closeMenuBtn: document.querySelector('[close-mobil-menu]'),
+    menuDiv: document.querySelector('[mobil-form]'),
+    menuLinks: document.querySelectorAll('.js-menu-link'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menuLinks.forEach(element => element.addEventListener('click', closeMenu));
+
+  function toggleModal() {
+    refs.modalDiv.classList.toggle('is-open');
+  }
+
+  function toggleMenu() {
+    refs.menuDiv.classList.toggle('is-open');
+  }
+
+  function closeMenu() {
+    refs.menuDiv.classList.remove('is-open');
+  }
+})();
